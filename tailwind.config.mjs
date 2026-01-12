a66c01b6a335d00a7e4cbdd4368fed86f06d1531
Michael Williams <michael.williams53@cfacorp.com>
2026-01-12 11:23:45 -0500

    Initial code commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,md}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
